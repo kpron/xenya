6d1fd8aac123f41635271c8062c36c38ce8b03e1
Kirill Prokhorenko <k_prokhorenko@wargaming.net>
2017-07-14 18:50:01 +0300

    url to env

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'https://xenya.herokuapp.com',
+        url: process.env.URL,
         mail: {},
         database: {
             client: 'postgres',
